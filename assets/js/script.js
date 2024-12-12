adfa1b36fab5b06a6ab72fed6349b16fe9d28c22
Isaac Nunez <isaacnunez1000@gmail.com>
2024-12-11 21:56:49 -0600

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,58 @@
 'use strict';
-
+ 
 mapboxgl.accessToken = 'pk.eyJ1Ijoid2VsbGZjIiwiYSI6ImNscTE5azY3eDAzeGwyaXIycTgyMnM0ZW8ifQ.0Bp9MRAu0DmdRpUI8lnDPg';
+
+const contactSection = document.querySelector('container flex flex-between');
+const orderNow = document.querySelector('image-container');
 
 const trackLocationButton = document.querySelector('.track-button');
 const mapContainer = document.querySelector('.tracking-map');
-
+ 
 trackLocationButton.addEventListener('click', () => {
-    navigator.geolocation.getCurrentPosition(success, error, accuracy);
+     navigator.geolocation.getCurrentPosition(success, error);
 });
-
-const accuracy = {
-    enableHighAccuracy:true
-}
-
+ 
 function success(position) {
-    const { latitude, longitude } = position.coords;
-
-    const map = new mapboxgl.Map({
-        container: mapContainer, 
-        style: 'mapbox://styles/mapbox/standard',
-        center: [longitude, latitude], 
-        zoom: 12 
-    });
-
-    new mapboxgl.Marker()
-        .setLngLat([longitude, latitude])
-        .addTo(map); 
+     const { latitude, longitude } = position.coords;
+   
+     const map = new mapboxgl.Map({ container: mapContainer,
+     style: 'mapbox://styles/mapbox/standard',
+     center: [longitude, latitude],
+    zoom: 10
+});
+ 
+new mapboxgl.Marker()
+  .setLngLat([longitude, latitude])
+  .addTo(map);
 }
-
+ 
 function error() {
-    console.error('Unable to retrieve location. Using default location.');
-
-    const defaultCoords = [-98.4936, 29.4241];
-
-    const map = new mapboxgl.Map({
-        container: mapContainer, 
-        style: 'mapbox://styles/mapbox/standard',
-        center: defaultCoords,
-        zoom: 12
-    });
-
-    new mapboxgl.Marker()
-        .setLngLat(defaultCoords)
-        .addTo(map);
+     console.error('Unable to retrieve location. Using default location.');
+     
+     
+     const defaultCoords = [-98.4936, 29.4241];
+     
+     const map = new mapboxgl.Map({
+         container: mapContainer,
+         style: 'mapbox://styles/mapbox/standard',
+         center: defaultCoords, zoom: 10
+});
+ 
+new mapboxgl.Marker()
+  .setLngLat(defaultCoords)
+  .addTo(map);
+}
+ 
+function scrollToContact() {
+     if (contactSection) {
+         contactSection.scrollIntoView( {
+             behavior: 'smooth' });
+        }
+}
+ 
+function scrollToImage() {
+     if (orderNow) {
+         orderNow.scrollIntoView( {
+             behavior: 'smooth' });
+        }
 }
